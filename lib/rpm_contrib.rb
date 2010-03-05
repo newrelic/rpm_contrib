dad1d1add3e94002f9c81c268401c67663579853
Bill Kayser <bkayser@newrelic.com>
2010-03-04 17:35:23 -0800

    Allow easier extensibility to new frameworks and dispatchers.

--- lib/rpm_contrib.rb
+++ lib/rpm_contrib.rb
@@ -1,17 +1,25 @@
-# Hook instrumentation and samplers in this gem into the normal RPM
-# start up sequence.
-require 'newrelic_rpm'
-module RPMContrib
-  
-  lib_root = File.dirname(__FILE__)
-  
-  # Tell the agent to load all the files in the rpm_contrib/instrumentation directory.
-  
-  NewRelic::Agent.add_instrumentation(lib_root+"/rpm_contrib/instrumentation/**/*.rb")
 
-  # Load all the Sampler class definitions.  These will register
-  # automatically with the agent.
-  Dir.glob(lib_root + "/rpm_contrib/samplers/**/*.rb") { |file| require file }
-  
-  VERSION = File.read(lib_root+"/../CHANGELOG")[/Version ([\d\.]+)$/, 1]
+RPM_CONTRIB_LIB = File.dirname(__FILE__)
+
+module RPMContrib
+  VERSION = File.read(RPM_CONTRIB_LIB+"/../CHANGELOG")[/Version ([\d\.]+)$/, 1]
 end
+
+
+# Perform any framework/dispatcher detection before loading the rpm
+# gem.
+
+raise "The rpm_contrib gem must be loaded before the newrelic_rpm gem." if defined?(::NewRelic)
+
+Dir.glob(RPM_CONTRIB_LIB + "/rpm_contrib/detection/**/*.rb") { |file| require file }
+
+require 'newrelic_rpm'
+
+# Tell the agent to load all the files in the rpm_contrib/instrumentation directory.
+
+NewRelic::Agent.add_instrumentation(RPM_CONTRIB_LIB+"/rpm_contrib/instrumentation/**/*.rb")
+
+# Load all the Sampler class definitions.  These will register
+# automatically with the agent.
+Dir.glob(RPM_CONTRIB_LIB + "/rpm_contrib/samplers/**/*.rb") { |file| require file }
+
