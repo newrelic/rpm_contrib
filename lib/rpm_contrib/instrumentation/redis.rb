12cbca729606841c16abca275a83ad8a988237ac
Ashley Martens <ashleym_72@yahoo.com>
2010-04-22 18:08:22 -0700

    Refine the Redis method tracing to match memcache instrumentation.

--- lib/rpm_contrib/instrumentation/redis.rb
+++ lib/rpm_contrib/instrumentation/redis.rb
@@ -1,15 +1,18 @@
 # == Redis Instrumentation
 #
 
-if defined?(::Redis) and not  NewRelic::Control.instance['disable_redis']
+if defined?(::Redis) and not  NewRelic::Control.instance['disable_redis_instrumentation']
   
   ::Redis.class_eval do 
     
     include NewRelic::Agent::MethodTracer
     
     def raw_call_command_with_newrelic_trace *args
-      self.class.trace_execution_scoped('Redis') do
-        # p "Instrumenting Redis Call!!!!:", args
+      method_name = args[0].is_a?(Array) ? args[0][0] : args[0]
+      metrics = ["Redis/#{method_name}",
+                 (NewRelic::Agent::Instrumentation::MetricFrame.recording_web_transaction? ? 'Redis/allWeb' : 'Redis/allOther')]
+      self.class.trace_execution_scoped(metrics) do
+        # NewRelic::Control.instance.log.debug("Instrumenting Redis Call[#{method_name}]: #{args[0].inspect}")
         raw_call_command_without_newrelic_trace(*args)
       end
     end
