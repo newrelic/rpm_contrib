05a1e1ecd2cdb69e6b02f22b44aeee4c0478be69
Ashley Martens <ashleym_72@yahoo.com>
2010-08-19 05:19:05 +0800

    Add Cassandra instrumentation.

---
+++ lib/rpm_contrib/instrumentation/cassandra.rb
@@ -0,0 +1,23 @@
+# == Cassandra Instrumentation
+#
+
+if defined?(::Cassandra) and not  NewRelic::Control.instance['disable_cassandra_instrumentation']
+  
+  ::Cassandra.class_eval do
+    add_method_tracer :insert,               'Database/Cassandra/insert'
+    add_method_tracer :remove,               'Database/Cassandra/remove'
+    add_method_tracer :clear_column_family!, 'Database/Cassandra/clear_column_family!'
+    add_method_tracer :clear_keyspace!,      'Database/Cassandra/clear_keyspace!'
+    add_method_tracer :count_columns,        'Database/Cassandra/count_columns'
+    add_method_tracer :multi_count_columns,  'Database/Cassandra/multi_count_columns'
+    add_method_tracer :get_columns,          'Database/Cassandra/get_columns'
+    add_method_tracer :multi_get_columns,    'Database/Cassandra/multi_get_columns'
+    add_method_tracer :get,                  'Database/Cassandra/get'
+    add_method_tracer :multi_get,            'Database/Cassandra/multi_get'
+    add_method_tracer :exists?,              'Database/Cassandra/exists?'
+    add_method_tracer :get_range,            'Database/Cassandra/get_range'
+    add_method_tracer :count_range,          'Database/Cassandra/count_range'
+    add_method_tracer :batch,                'Database/Cassandra/batch'
+  end
+
+end
