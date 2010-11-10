ff711f6654d6eaa547fbd64f04340a3b49650ccc
Ben Poweski <bpoweski@gmail.com>
2010-11-11 01:34:23 +0800

    add if defined? guards for JSON/XML for crack

--- lib/rpm_contrib/instrumentation/crack.rb
+++ lib/rpm_contrib/instrumentation/crack.rb
@@ -1,15 +1,19 @@
 if defined?(::Crack) && !NewRelic::Control.instance['disable_crack']
-  Crack::JSON.class_eval do
-    class << self
-      include NewRelic::Agent::MethodTracer
-      add_method_tracer :parse, 'Parser/#{self.class.name}/parse'
+  if defined?(::Crack::JSON)
+    ::Crack::JSON.class_eval do
+      class << self
+        include NewRelic::Agent::MethodTracer
+        add_method_tracer :parse, 'Parser/#{self.class.name}/parse'
+      end
     end
   end
 
-  Crack::XML.class_eval do
-    class << self
-      include NewRelic::Agent::MethodTracer
-      add_method_tracer :parse, 'Parser/#{self.class.name}/parse'
+  if defined?(::Crack::XML)
+    ::Crack::XML.class_eval do
+      class << self
+        include NewRelic::Agent::MethodTracer
+        add_method_tracer :parse, 'Parser/#{self.class.name}/parse'
+      end
     end
   end
 end
