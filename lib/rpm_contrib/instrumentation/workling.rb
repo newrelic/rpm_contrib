66616a92e44465eed161c444913fe7adae78fe31
Scott Fleckenstein <nullstyle@gmail.com>
2011-01-05 07:10:23 +0800

    fix simple typo

--- lib/rpm_contrib/instrumentation/workling.rb
+++ lib/rpm_contrib/instrumentation/workling.rb
@@ -9,7 +9,7 @@ if defined?(::Workling) and not NewRelic::Control.instance['disable_workling']
   Workling::Discovery.discovered.each do |clazz|
     (clazz.public_instance_methods - Workling::Base.public_instance_methods).each do |method|
       puts "added method tracer Workling/#{klazz.name}/#{method}"
-      clazz.send(:add_method_tracer, method, "Workling/#{klazz.name}/#{method}", :category => :task)
+      clazz.send(:add_method_tracer, method, "Workling/#{clazz.name}/#{method}", :category => :task)
     end
   end
 end
