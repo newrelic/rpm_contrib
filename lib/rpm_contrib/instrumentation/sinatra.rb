922b7357b6ebdffc79d182ba23bb61856ed8876f
Paul Ingles <paul@oobaloo.co.uk>
2011-03-08 20:52:13 +0800

    Add sinatra template tracing; can be disabled with disable_sinatra_template

---
+++ lib/rpm_contrib/instrumentation/sinatra.rb
@@ -0,0 +1,20 @@
+if defined?(Sinatra::Base) and not NewRelic::Control.instance['disable_sinatra_template']
+  module Sinatra
+    Base.class_eval do
+      def render_with_newrelic_trace(*args, &block)
+        engine, file = *args
+        return render_without_newrelic_trace(*args, &block) if file == "= yield"
+
+        file = "Proc" if file.is_a?(Proc)        
+        metrics = ["Sinatra/#{engine}/#{file}"]
+        
+        self.class.trace_execution_scoped metrics do
+          render_without_newrelic_trace(*args, &block)
+        end
+      end
+      
+      alias render_without_newrelic_trace render
+      alias render render_with_newrelic_trace
+    end
+  end
+end  
