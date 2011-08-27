02862bd62bad90dcb3a6388ceaf92750f89ae643
Chris Griego <cgriego@gmail.com>
2011-08-26 23:48:25 -0500

    Fix Yajl instrumentation to trace Yajl::Encoder#encode and Yajl::Parser#parse

--- lib/rpm_contrib/instrumentation/yajl.rb
+++ lib/rpm_contrib/instrumentation/yajl.rb
@@ -1,13 +1,28 @@
 DependencyDetection.defer do
   depends_on do
-    defined?(::Yajl::Encoder) and not ::NewRelic::Control.instance['disable_yajl_instrumentation']
+    defined?(::Yajl::Parser) && !NewRelic::Control.instance['disable_yajl_instrumentation']
+  end
+
+  executes do
+    ::Yajl::Parser.class_eval do
+      class << self
+        include ::NewRelic::Agent::MethodTracer
+        add_method_tracer :parse, 'Parser/Yajl/parse'
+      end
+    end
+  end
+end
+
+DependencyDetection.defer do
+  depends_on do
+    defined?(::Yajl::Encoder) && !NewRelic::Control.instance['disable_yajl_instrumentation']
   end
 
   executes do
     ::Yajl::Encoder.class_eval do
       class << self
         include ::NewRelic::Agent::MethodTracer
-        add_method_tracer :parse, 'Encoder/Yajl/encode'
+        add_method_tracer :encode, 'Encoder/Yajl/encode'
       end
     end
   end
