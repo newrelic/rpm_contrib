0020ccb5121652a264e176380713dd364142003e
W. Andrew Loe III <andrew@andrewloe.com>
2011-11-07 14:45:59 -0800

    Instrumentation for ThinkingSphinx.

---
+++ lib/rpm_contrib/instrumentation/thinking_sphinx.rb
@@ -0,0 +1,21 @@
+DependencyDetection.defer do
+  @name = :thinking_sphinx
+    
+  depends_on do
+    defined?(::ThinkingSphinx) and not ::NewRelic::Control.instance['disable_thinking_sphinx']
+  end
+  
+  executes do
+    NewRelic::Agent.logger.debug 'Installing Thinking Sphinx instrumentation'
+  end
+  
+  executes do
+    class  ::ThinkingSphinx::Search
+      include NewRelic::Agent::MethodTracer
+
+      add_method_tracer :populate
+      add_method_tracer :search
+    end
+  end
+end
+
