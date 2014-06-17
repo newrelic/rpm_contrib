34e434e1c0313339ec9bea1cf2f105633070d24d
Jason R. Clark <jclark@newrelic.com>
2014-06-17 16:39:43 -0700

    rpm_contrib is deprecated

--- lib/rpm_contrib/detection.rb
+++
@@ -1,5 +0,0 @@
-# Currently the only framework we are detecting with an agent
-# patch is camping.  You need to specify the camping gem prior
-# to the contrib gem and newrelic agent gem in order for this to work.
-require 'rpm_contrib/detection/camping'
-
--- test/helper.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require 'test/unit'
-
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-
-require File.expand_path("../../lib/rpm_contrib.rb", __FILE__)
-
-require 'schema.rb'
